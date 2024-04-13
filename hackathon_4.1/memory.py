93af8992daba24b9042c73bcd2cf00fecb5b28a9
gurusaiedu <gurusai.edu@gmail.com>
2024-04-13 22:01:39 +0530

    secode commit
    Signed-off-by: gurusaiedu <gurusai.edu@gmail.com>

---
+++ hackathon_4.1/memory.py
@@ -0,0 +1,48 @@
+from langchain.memory import ConversationBufferMemory
+
+memory=ConversationBufferMemory()
+
+def add_to_memory(response,user_input=""):
+    if not memory.buffer and user_input=="":
+      memory.save_context({"input":"IMG"},
+                          {"ouput":response})
+    else:
+      memory.save_context({"input":user_input},
+                         {"ouput":response})
+    
+
+def chat_memory():
+  r=memory.load_memory_variables({})
+  history=r['history']
+  chat_array=history.split("\n")
+
+  t=0
+  h=""
+  a=""
+  for i in chat_array:
+    if i.startswith("Human"):
+      t=1
+      # h=h+i
+    elif i.startswith("AI"):
+      t=2
+      # a=a+i
+    if t==1:
+      h=h+i
+    elif t==2:
+      a=a+i
+
+  a1=a.split("AI:")
+  h1=h.split("Human:")
+
+  ll=[]
+  for i in range(len(a1)):
+    if i>0 :
+      a2="AI:"+a1[i]
+      h2="Human:"+h1[i]
+      ll.append(h2)
+      ll.append(a2)
+
+  return ll
+
+
+
---
+++ hackathon_4.1/pdf_chain.py
@@ -0,0 +1,137 @@
+import io
+import re
+import base64
+
+from IPython.display import HTML, display
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from PIL import Image
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI  
+from langchain_core.documents import Document
+
+
+
+def plt_img_base64(img_base64):
+    """Disply base64 encoded string as image"""
+    # Create an HTML img tag with the base64 string as the source
+    image_html = f''
+    # Display the image by rendering the HTML
+    display(HTML(image_html))
+
+
+def looks_like_base64(sb):
+    """Check if the string looks like base64"""
+    return re.match("^[A-Za-z0-9+/]+[=]{0,2}$", sb) is not None
+
+
+def is_image_data(b64data):
+    """
+    Check if the base64 data is an image by looking at the start of the data
+    """
+    image_signatures = {
+        b"\xFF\xD8\xFF": "jpg",
+        b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A": "png",
+        b"\x47\x49\x46\x38": "gif",
+        b"\x52\x49\x46\x46": "webp",
+    }
+    try:
+        header = base64.b64decode(b64data)[:8]  # Decode and get the first 8 bytes
+        for sig, format in image_signatures.items():
+            if header.startswith(sig):
+                return True
+        return False
+    except Exception:
+        return False
+
+
+def resize_base64_image(base64_string, size=(128, 128)):
+    """
+    Resize an image encoded as a Base64 string
+    """
+    # Decode the Base64 string
+    img_data = base64.b64decode(base64_string)
+    img = Image.open(io.BytesIO(img_data))
+
+    # Resize the image
+    resized_img = img.resize(size, Image.LANCZOS)
+
+    # Save the resized image to a bytes buffer
+    buffered = io.BytesIO()
+    resized_img.save(buffered, format=img.format)
+
+    # Encode the resized image to Base64
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+
+def split_image_text_types(docs):
+    """
+    Split base64-encoded images and texts
+    """
+    b64_images = []
+    texts = []
+    for doc in docs:
+        # Check if the document is of type Document and extract page_content if so
+        if isinstance(doc, Document):
+            doc = doc.page_content
+        if looks_like_base64(doc) and is_image_data(doc):
+            doc = resize_base64_image(doc, size=(1300, 600))
+            b64_images.append(doc)
+        else:
+            texts.append(doc)
+    return {"images": b64_images, "texts": texts}
+
+
+def img_prompt_func(data_dict):
+    """
+    Join the context into a single string
+    """
+    formatted_texts = "\n".join(data_dict["context"]["texts"])
+    messages = []
+
+    # Adding image(s) to the messages if present
+    if data_dict["context"]["images"]:
+        for image in data_dict["context"]["images"]:
+            image_message = {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{image}"},
+            }
+            messages.append(image_message)
+
+    # Adding the text for analysis
+    text_message = {
+        "type": "text",
+        "text": (
+            #"You are  analyst tasking with providing investment advice.\n"
+            "You will be given a mixed of text, tables, and image(s) usually of charts or graphs.\n"
+            "Use this information to provide response related to the user question. \n"
+            f"User-provided question: {data_dict['question']}\n\n"
+            "Text and / or tables:\n"
+            f"{formatted_texts}"
+        ),
+    }
+    messages.append(text_message)
+    return [HumanMessage(content=messages)]
+
+from langchain_core.output_parsers import StrOutputParser
+
+def multi_modal_rag_chain(retriever):
+    """
+    Multi-modal RAG chain
+    """
+
+    # Multi-modal LLM
+    #model = model #ChatOpenAI(temperature=0, model="gpt-4-vision-preview", max_tokens=1024)
+    model=ChatGoogleGenerativeAI(model='gemini-pro-vision')
+
+    # RAG pipeline
+    chain = (
+        {
+            "context": retriever | RunnableLambda(split_image_text_types),
+            "question": RunnablePassthrough(),
+        }
+        | RunnableLambda(img_prompt_func)
+        | model
+        | StrOutputParser()
+    )
+    return chain
+
